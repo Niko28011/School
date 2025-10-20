c76aa3315baa672ed1d746325a4ccf6c8df6948c
Nikolaj <Niko28011@gmail.com>
2025-10-20 19:10:27 +0200

    Update Disaheim project

--- Disaheim/Disaheim/Book.cs
+++ Disaheim/Disaheim/Book.cs
@@ -8,8 +8,29 @@ namespace Disaheim
 {
     public class Book
     {
-        public string ItemId;
-        public string Title;
-        public double Price;
+        // Properties 
+        public string ItemId { get; set; }
+        public string Title { get; set; }
+        public double Price { get; set; }
+
+        public Book(string itemId)
+        {
+            ItemId = itemId;
+        }
+        public Book(string itemId, string title) : this(itemId)
+        {
+            Title = title;
+        }
+        public Book(string itemId, string title, double price) : this(itemId, title)
+        {
+            Price = price;
+        }
+        public override string ToString()
+        {
+            return $"ItemId: {ItemId}, Title: {Title}, Price: {Price}";
+        }
+
     }
+
+
 }
