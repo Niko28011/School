02cd30f74f187025e4e8a3ae62064ca40d8ea659
Nikolaj <Niko28011@gmail.com>
2025-10-20 19:24:10 +0200

    Update

---
+++ Disaheim/Disaheim/Controller.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+    public class Controller
+    {
+        // Properties (Lister af bøger og amuletter)
+        public List<Book> Books { get; set; }
+        public List<Amulet> Amulets { get; set; }
+
+        // Constructor
+        public Controller()
+        {
+            Books = new List<Book>();
+            Amulets = new List<Amulet>();
+        }
+
+        //Overloaded AddToList() metoder
+        public void AddToList(Book book)
+        {
+            Books.Add(book);
+        }
+
+        public void AddToList(Amulet amulet)
+        {
+            Amulets.Add(amulet);
+        }
+    }
+}
