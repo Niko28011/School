63d687df4a5b6f52f3f6975857ddba0f2e02f14a
Nikolaj <Niko28011@gmail.com>
2025-10-20 18:12:24 +0200

    Update to Disaheim project

---
+++ Disaheim/Disaheim/Amulet.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+    public enum Level
+    {
+        Low,
+        Medium,
+        High
+    }
+    public class Amulet
+    {
+        public string ItemId;
+        public string Design;
+        public Level Quality;
+
+        //Constructor
+        public Amulet(string itemId)
+        {
+            ItemId = itemId;
+            Quality = Level.Medium; // standardværdi
+        }
+        public Amulet(string itemId, Level quality) : this(itemId)
+        {
+            Quality = quality;
+        }
+        public Amulet(string itemId, Level quality, string design) : this(itemId, quality)
+        {
+            Design = design;
+        }
+        public override string ToString()
+        {
+            return $"ItemId: {ItemId}, Quality: {Quality}, Design: {Design}";
+        }
+
+    }
+}
